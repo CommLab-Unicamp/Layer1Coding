416d103ff370b2eee394f2aa73f0f42641eb77be
Thiago <thipavao44@gmail.com>
2024-11-19 08:04:41 -0300

    comparacao de caractere

--- tests-ldpc/interactive.py
+++ tests-ldpc/interactive.py
@@ -1,9 +1,12 @@
 from test_pyldpc import *
 
+# Inicializa as matrizes H e G
 H, G = initialize_matrices()
 
+# Define a mensagem padrão
 default_message = "Esta é uma mensagem de texto, que deve conter 140 caracteres. Pode não parecer, mas 140 caracteres é bastante coisa. Viu só? Enrolei mas foi"
 
+# Solicita a entrada do usuário
 msg = input("Digite a mensagem de teste a ser enviada (max 140 caracteres), ou deixe em branco para uma mensagem padrão.\n\nMensagem > ")
 
 # Verifica se a mensagem tem mais de 140 caracteres
@@ -13,13 +16,14 @@ elif len(msg) > 140:
     print("Erro: A mensagem excede o limite de 140 caracteres.")
     exit()
 
+# Exibe informações da mensagem
 print("\nTamanho da mensagem a ser enviada (caracteres):", len(msg))
-print(f"\nMensagem: \"{msg}\"")
+print(f"\nMensagem: \"{msg}\"\n")
 
 # Solicita a variância do ruído gaussiano
 variance = float(input("Digite agora a variância do ruído gaussiano no canal discreto: "))
 
-print("Enviando mensagem...\n\n")
+print("\nEnviando mensagem...\n")
 
 # Calcula o SNR a partir da variância
 snr_db = variance_to_SNR_db(variance)
@@ -27,17 +31,13 @@ snr_db = variance_to_SNR_db(variance)
 # Envia a mensagem e recebe a mensagem decodificada
 received_msg = transmit_message(msg, G, H, snr_db)
 
-# Converte a mensagem original e a mensagem recebida em arrays de bits
-original_bits = string_to_numpy_bitarray(msg)
-received_bits = string_to_numpy_bitarray(received_msg)
+# Calcula o número de erros de caractere
+num_char_errors = sum(1 for a, b in zip(msg, received_msg) if a != b)
 
-# Calcula o número de erros de bits
-num_errors = np.sum(original_bits != received_bits)
+# Calcula a taxa de erro de caracteres
+char_error_rate = num_char_errors / len(msg) * 100
 
-# Calcula a taxa de erro de bits (BER)
-ber = num_errors / len(original_bits) * 100
-
-# Exibe a mensagem recebida e a taxa de erro
+# Exibe a mensagem recebida e a taxa de erro de caracteres
 print(f"\nMensagem recebida: \"{received_msg}\"")
-print(f"Quantidade de erros de caractere: {num_errors}")
-print(f"Taxa de erro de bits (BER): {ber:.2f}%")
+print(f"\nQuantidade de erros de caracteres: {num_char_errors}")
+print(f"Taxa de erro de caracteres: {char_error_rate:.2f}%")
